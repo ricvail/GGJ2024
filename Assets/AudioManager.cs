65d71f6be1c7777f16e361b685980831d6ee701c
Gianluca <gianlucasara95@gmail.com>
2024-01-27 23:12:04 +0100

    AudioManager

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -6,8 +6,8 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
     
-    public Sound[] musicSounds, sfxSounds;
-    public AudioSource musicSource, sfxSource;
+    public Sound[] musicSounds, sfxSounds, ambSounds;
+    public AudioSource musicSource, sfxSource, ambSource;
 
     private void Awake()
     {
@@ -20,7 +20,13 @@ public class AudioManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        
+    }
+
+    private void Start()
+    {
         PlayMusic("Music_Gameplay");
+        PlayAmb("Crowd");
     }
 
     public void PlayMusic(string name)
@@ -52,6 +58,22 @@ public class AudioManager : MonoBehaviour
             sfxSource.volume = s.volume;
             sfxSource.pitch = s.pitch;
             sfxSource.PlayOneShot(s.clip);
+        }
+    }
+
+    public void PlayAmb(string name)
+    {
+        Sound s = Array.Find(ambSounds, x => x.name == name);
+        if (s == null)
+        {
+            Debug.Log("Sound not Found");
+        }
+        else
+        {
+            ambSource.clip = s.clip;
+            ambSource.volume = s.volume;
+            ambSource.pitch = s.pitch;
+            ambSource.Play();
         }
     }
 }
