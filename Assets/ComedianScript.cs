4f55df6b4aec31dc7de1b8e3b50bff8ef87e4c15
Riccardo Vailati <ricvail@hotmail.it>
2024-01-28 14:14:10 +0100

    level 1 movement, fixed cutscene

---
+++ Assets/ComedianScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ComedianScript : MonoBehaviour
+{
+
+    public Animator curtains;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Throwable"))
+        {
+            curtains.SetBool("isGameOver", true);
+            StartCoroutine(delayedLoadScene(3));
+        }
+    }
+
+    IEnumerator delayedLoadScene(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Animation over");
+        CutsceneController.nextSceneName = "Level2";
+        CutsceneController.joke = "I am addicted to drinking brake fluid! But it's okay, I can stop any time.";
+        SceneManager.LoadScene("Cutscene");
+    }
+}
